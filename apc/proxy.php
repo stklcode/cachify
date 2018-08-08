20a6d9169de4ae0ac53a072e59002916616d6b85
Stefan Kalscheuer <stefan@stklcode.de>
2018-08-08 20:31:05 +0200

    Added APC Proxy to code style check

--- apc/proxy.php
+++ apc/proxy.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * Cachify: APC caching backend.
+ *
+ * This file contains the backend class for APC caching.
+ *
+ * @package   Cachify
+ */
 
 if ( ! empty( $_COOKIE ) ) {
 	foreach ( $_COOKIE as $k => $v ) {
@@ -37,16 +44,19 @@ if (
 	&& ( strpos( filter_input( INPUT_SERVER, 'PHP_SELF', FILTER_SANITIZE_STRING ), '/wp-admin/' ) === false )
 	&& ( strpos( filter_input( INPUT_SERVER, 'HTTP_ACCEPT_ENCODING', FILTER_SANITIZE_STRING ), 'gzip' ) !== false )
 	&& extension_loaded( 'apc' )
-	&& ( $cache = apc_fetch( md5( ( cachify_is_ssl() ? 'https-' : '' ) . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'] ) . '.cachify' ) )
 ) {
-	ini_set( 'zlib.output_compression', 'Off' );
+	// Fetch from cache.
+	$cache = apc_fetch( md5( ( cachify_is_ssl() ? 'https-' : '' ) . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'] ) . '.cachify' );
+	if ( $cache ) {
+		ini_set( 'zlib.output_compression', 'Off' );
 
-	header( 'Vary: Accept-Encoding' );
-	header( 'X-Powered-By: Cachify' );
-	header( 'Content-Encoding: gzip' );
-	header( 'Content-Length: ' . strlen( $cache ) );
-	header( 'Content-Type: text/html; charset=utf-8' );
+		header( 'Vary: Accept-Encoding' );
+		header( 'X-Powered-By: Cachify' );
+		header( 'Content-Encoding: gzip' );
+		header( 'Content-Length: ' . strlen( $cache ) );
+		header( 'Content-Type: text/html; charset=utf-8' );
 
-	echo $cache;
-	exit;
+		echo $cache;    // phpcs:ignore
+		exit;
+	}
 }
