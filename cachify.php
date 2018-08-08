d0d6ae6c391b2722e166fa1762524c052c085e95
Stefan Kalscheuer <stefan@stklcode.de>
2018-08-08 20:36:33 +0200

    Renamed class filed according to WPCS and adapted autoloader

--- cachify.php
+++ cachify.php
@@ -91,9 +91,9 @@ function cachify_autoload( $class ) {
 	if ( in_array( $class, array( 'Cachify', 'Cachify_APC', 'Cachify_DB', 'Cachify_HDD', 'Cachify_MEMCACHED' ) ) ) {
 		require_once(
 			sprintf(
-				'%s/inc/%s.class.php',
+				'%s/inc/class-%s.php',
 				CACHIFY_DIR,
-				strtolower( $class )
+				strtolower( str_replace( '_', '-', $class ) )
 			)
 		);
 	}
